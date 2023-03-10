7b35bceae7838597356e2d444bd326138f1be753
Joao Moreira <jgmoreira1809@gmail.com>
2023-03-09 21:00:07 -0300

    Req 01 e 02 OK

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,24 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(data):
+
+        oldest_date = min(product["data_de_fabricacao"] for product in data)
+
+        today_date = datetime.today().isoformat()
+        closest_expiration_date = min(
+            product["data_de_validade"] for product in data
+            if product["data_de_validade"] > today_date)
+
+        count = Counter(product["nome_da_empresa"] for product in data)
+
+        more_products_company = max(count, key=count.get)
+
+        return (
+            f"Data de fabricação mais antiga: {oldest_date}\n"
+            f"Data de validade mais próxima: {closest_expiration_date}\n"
+            f"Empresa com mais produtos: {more_products_company}"
+        )
--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,29 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    id = 12345
+    nome_da_empresa = "ACME Inc."
+    nome_produto = "Martelo"
+    data_fabricacao = "2022-01-01"
+    data_validade = "2023-01-01"
+    numero_serie = "ABC123XYZ"
+    instrucoes_armazenamento = "Armazenar em local seco e arejado"
+
+    produto = Product(
+        id=id,
+        nome_da_empresa=nome_da_empresa,
+        nome_do_produto=nome_produto,
+        data_de_fabricacao=data_fabricacao,
+        data_de_validade=data_validade,
+        numero_de_serie=numero_serie,
+        instrucoes_de_armazenamento=instrucoes_armazenamento
+    )
+
+    assert produto.id == id
+    assert produto.nome_da_empresa == nome_da_empresa
+    assert produto.nome_do_produto == nome_produto
+    assert produto.data_de_fabricacao == data_fabricacao
+    assert produto.data_de_validade == data_validade
+    assert produto.numero_de_serie == numero_serie
+    assert produto.instrucoes_de_armazenamento == instrucoes_armazenamento
