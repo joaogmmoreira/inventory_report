c63a6422181ea54f2726060d26d30671f222cba9
Joao Moreira <jgmoreira1809@gmail.com>
2023-03-12 14:18:16 -0300

    Req 03 OK

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,24 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+# from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+
+    @classmethod
+    def generate(self, data):
+        simple_report = super().generate(data)
+        companies = {}
+        for company in data:
+            if company["nome_da_empresa"] in companies:
+                companies[company["nome_da_empresa"]] += 1
+            else:
+                companies[company["nome_da_empresa"]] = 1
+
+        companies_list = ""
+        for company, quantity in companies.items():
+            companies_list += f"- {company}: {quantity}\n"
+
+        return (
+            f"{simple_report}\n"
+            f"Produtos estocados por empresa:\n{companies_list}"
+        )
